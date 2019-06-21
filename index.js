1e44609160217514d90bd51eceb80ab90b768dd1
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-21 21:55:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ var string = musicians[i] + " plays " + instruments[i] ;
 }
 theBeatlesPlay();
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function johnLennonFacts(facts){
   var i = 0;
@@ -24,6 +30,6 @@ function johnLennonFacts(facts){
   lennonArray[i] = facts[i] +"!!!";
   i++;
 }
-  
+  return lennonArray;
 }
 johnLennonFacts();
